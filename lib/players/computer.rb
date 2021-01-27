7f5a954ebbf0b1c2d1667d8dcd20a715bc77956b
Ben Hayes <b.t.hayes@comcast.net>
2021-01-27 23:36:07 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,13 +3,33 @@ module Players
     
     def move(board)
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-      move = valid_moves.sample
-        if board.valid_move?(move)
-          valid_moves = valid_moves.delete(move)
-          valid_moves
-          move  
-        end
+      #move = valid_moves.sample
+      #  if board.valid_move?(move)
+      #    valid_moves = valid_moves.delete(move)
+      #    valid_moves
+      #    move  
+      #  end
+      if board.cells.size == 0 
+        if board.valid_move?("5")
+          "5"
+        end 
+        elsif board.taken?("5")
+          move = ["1", "3", "7", "9"].sample
+          if board.valid_move?(move)
+            move
+          end 
+          else 
+            move = valid_moves.sample
+            if board.valid_move?(move)
+              move 
+            end
+      end
     end 
+            
+            
+        
+ 
+
     
   end 
 end 
