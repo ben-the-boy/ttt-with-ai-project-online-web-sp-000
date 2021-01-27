5de122f12ecb330d4b8ea2f71388a3f6bb4b1c28
Ben Hayes <b.t.hayes@comcast.net>
2021-01-27 23:49:34 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,8 +2,8 @@ module Players
   class Computer < Player
     
     def move(board)
-      valid_moves = []
-      #valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      corners = 
       #move = valid_moves.sample
       #  if board.valid_move?(move)
       #    valid_moves = valid_moves.delete(move)
