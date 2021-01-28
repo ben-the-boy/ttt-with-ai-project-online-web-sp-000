90a2022cfaa864448201bb78d70983f72593d675
Ben Hayes <b.t.hayes@comcast.net>
2021-01-28 00:03:54 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,36 +4,12 @@ module Players
     def move(board)
       valid_moves = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
       corners = ["1", "3", "7", "9"]
-      #move = valid_moves.sample
-      #  if board.valid_move?(move)
-      #    valid_moves = valid_moves.delete(move)
-      #    valid_moves
-      #    move  
-      #  end
-      if board.valid_move?("5")
-        return "5"
-      end 
-      if board.taken?("5")
-          corners.each do |move|
-            if board.valid_move?(move)
-              move
-            end
-            move 
-          end 
-          else 
-            valid_moves.each do |move|
-            if board.valid_move?(move)
-              move  
-            end
-            move 
-          end 
-      end
-    end 
-            
-            
-        
- 
+      move = valid_moves.sample
+        if board.valid_move?(move)
+          valid_moves = valid_moves.delete(move)
+          valid_moves
+          move  
+        end
 
-    
   end 
 end 
